604f5d51d168c5770707cbe9ba4373c626e3fe88
Dhanush <suvarnadhanush2000@gmail.com>
2023-08-16 00:05:52 +0530

    update: Add content to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./**/*.{liquid,json}"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
